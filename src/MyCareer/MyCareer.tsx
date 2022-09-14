a1efe60a6988c2fce870083d6ba61595e29d04bd
Wesley <brooks.wesley1217@gmail.com>
2022-09-14 11:19:08 -0400

    MN4-FRONTEND-001
    Completion of user story 001 for the front end. Includes extra components of RefreshCardSection used inside of box 1 of the MyCareer page. Not currently updated for state management.
    WB

---
+++ src/MyCareer/MyCareer.tsx
@@ -0,0 +1,20 @@
+import CollegeCard from "../GlobalComponents/CollegeCard";
+import CareerDynamicBox from "./CareerDynamicBox";
+import './MyCareer.css';
+import CarouselCustom from "../GlobalComponents/CarouselCustom";
+import RefreshCardSection from "../GlobalComponents/RefreshCardSection";
+
+function MyCareer () {
+    return (
+        <div> 
+            <div className="box"> 
+                <CareerDynamicBox />
+            </div>
+            
+            
+            
+        </div>
+    )
+}
+
+export default MyCareer;
