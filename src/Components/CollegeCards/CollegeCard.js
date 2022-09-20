52d498c2039a4fee80631ab6a55a738d5c54396b
B3krodge <106502294+B3krodge@users.noreply.github.com>
2022-09-20 13:04:05 -0400

    Add shell

--- src/Components/CollegeCards/CollegeCard.js
+++ src/Components/CollegeCards/CollegeCard.js
@@ -6,31 +6,44 @@ import TuitionCost from "./Components/TuitionCost";
 import Summary from "./Components/Summary";
 import Recommendations from "./Components/Recommendations";
 import AvgEarnings from "./Components/AvgEarnings";
+import ColorShell from "../ColorShell";
+import { Paper } from "@mantine/core";
+import Next4Shell from "../Next4Shell";
 
 //https://ui.mantine.dev/category/stats
 function CollegeCard() {
   return (
-    <div className="college-card">
-      <div class="college-card_image">
-        <img src={collegepic}></img>
-      </div>
-      <div className="college-card_applicants">
-        <Piechart></Piechart>
-      </div>
-      <div>
-        <Summary></Summary>
-      </div>
-      <div>
-        <AvgEarnings></AvgEarnings>
-      </div>
-      <container className="college-card_tuition">
-        <h1>Tuition Cost</h1>
-        <TuitionCost></TuitionCost>
-      </container>
-      <div>
-        <Recommendations></Recommendations>
-      </div>
-    </div>
+    <ColorShell
+      innerContent={
+        <Paper>
+          <Next4Shell
+            page={
+              <div className="college-card">
+                <div class="college-card_image">
+                  <img src={collegepic}></img>
+                </div>
+                <div className="college-card_applicants">
+                  <Piechart></Piechart>
+                </div>
+                <div>
+                  <Summary></Summary>
+                </div>
+                <div>
+                  <AvgEarnings></AvgEarnings>
+                </div>
+                <container className="college-card_tuition">
+                  <h1>Tuition Cost</h1>
+                  <TuitionCost></TuitionCost>
+                </container>
+                <div>
+                  <Recommendations></Recommendations>
+                </div>
+              </div>
+            }
+          ></Next4Shell>
+        </Paper>
+      }
+    ></ColorShell>
   );
 }
 
