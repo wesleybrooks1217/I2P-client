b309750103492cec7e4d7b7e1388ea84c14b78f0
Drake Griffith <Drakegriffith8@gmail.com>
2022-10-09 16:23:35 -0400

    excess landing removal

---
+++ src/Components/Dashboard/Components/Homeroom/Homeroom.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Homeroom.css"
+
+function Homeroom() {
+    return (
+        <div className='container'>
+                    Test
+        </div>
+    )
+}
--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -41,63 +41,21 @@ import ColorShell from '../ColorShell';
 import UserProfile from './Components/UserProfile';
 import Notifications from './Components/Notifications';
 import { render } from '@testing-library/react';
-
+import Homeroom from "./Components/Homeroom/Homeroom"
 function Dashboard() {
-  const [renderedState, setRenderedState] = useState('');
-  useEffect(() => {
-    
-  }, [renderedState])
-
-  function handleChange(name) {
-   
-    setRenderedState(name)
-    console.log(renderedState)
-  
-  };
-
-
-
   return (
     <div className="App">
        <ColorShell innerContent={
         <Paper>
         <Next4Shell
           page= { 
-
-      
-          <div >
-                <Container sx={{marginTop: '150px', width: 900, marginBottom: 0, display: 'flex', justifyContent: 'left', alignItems: 'baseline'}}>
-            <h1> John Calvert </h1>
-            <p style={{marginLeft: 10}}> Cannon Cougars </p> 
+              <Homeroom />
           
-          </Container>
-           
             
-          
-            <Container sx={{width: 900, marginLeft: 'auto', marginRight: 'auto',  marginBottom: 20, display: 'flex', alignItems: 'center'}}>
-            <UserProfile />
-               
-                </Container>
-             
-                
-              
   
-      
-          <Container sx={{width: 900, marginBottom: 0, display: 'flex', justifyContent: 'left', alignItems: 'center'}}>
-            <h1> MyDashboard </h1>
-          
-          </Container>
-         
-          <Container mt={0} sx={{width: '500px' ,justifyContent: 'center', display: 'flex'}}>
-            <MyMissions handleChange={handleChange} />
-  
-            <Box nameBox={renderedState}
-                  />
-            </Container>
-           
-          <MyMap />
+     
 
-          </div>
+        
          }
          />
        </Paper>
