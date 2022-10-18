0c6b853298ebcd0de7988dd2a4a2f3952986b9a7
Drake Griffith <Drakegriffith8@gmail.com>
2022-10-18 14:35:35 -0400

    Update userActions.jsx

--- src/redux/users/userActions.jsx
+++ src/redux/users/userActions.jsx
@@ -1,18 +1,18 @@
 
-const fetchUsersRequest = () => {
+export const fetchUsersRequest = () => {
     return {
         type: FETCH_USERS_REQUEST
     }
 }
 
-const fetchUsersSuccess = users => {
+export const fetchUsersSuccess = users => {
     return {
         type: FETCH_USERS_SUCCESS,
         payload: users
     }
 }
 
-const fetchUsersFailure = error => {
+export const fetchUsersFailure = error => {
     return {
         type: FETCH_USERS_FAILURE,
         payload: error
