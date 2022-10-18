40087a7b12135b18903ed63b663de08d4c4495f1
Drake Griffith <Drakegriffith8@gmail.com>
2022-10-18 14:30:36 -0400

    wrong location

--- redux/store.js
+++
@@ -1,9 +0,0 @@
-import {createStore, applyMiddleware} from 'redux'
-import thunk from 'redux-thunk'
-import rootReducer from './rootReducer'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import logger from 'redux-logger'
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, thunk)))
-
-export default store
