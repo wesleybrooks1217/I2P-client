4b4dda2b4c54c83bac7000e243132c939a1fee9e
Drake Griffith <Drakegriffith8@gmail.com>
2022-10-18 14:33:54 -0400

    update users

---
+++ src/redux/users/userActions.jsx
@@ -0,0 +1,37 @@
+
+const fetchUsersRequest = () => {
+    return {
+        type: FETCH_USERS_REQUEST
+    }
+}
+
+const fetchUsersSuccess = users => {
+    return {
+        type: FETCH_USERS_SUCCESS,
+        payload: users
+    }
+}
+
+const fetchUsersFailure = error => {
+    return {
+        type: FETCH_USERS_FAILURE,
+        payload: error
+    }
+}
+
+
+const fetchUsers = () => {
+    return function(dispatch) {
+        dispatch(fetchUsersRequest())
+        axios.get("http://127.0.0.1:8000/api/User/")
+            .then(response => {
+                // response.data is the array of users
+                const users = response.data.map(user => user.id )
+                dispatch(fetchUsersSuccess(users))
+            })
+            .catch(error => {
+                dispatch(fetchUsersFailure(error.message))
+                // error.message is the error description
+            })
+    }
+}
