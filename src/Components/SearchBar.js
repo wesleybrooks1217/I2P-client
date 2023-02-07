d84a6bc502d41a534c3d4e4c0c126106e4504cd7
B3krodge <106502294+B3krodge@users.noreply.github.com>
2023-02-07 12:44:19 -0500

    Update SearchBar.js

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -8,15 +8,47 @@ function SearchBar() {
   }
 
   return (
-    <>
+    <div style={styles.container}>
       <input
+        style={styles.input}
         type="text"
         value={searchTerm}
-        onChange={e => setSearchTerm(e.target.value)}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <button onClick={handleSearch}>Search</button>
-    </>
+      <button style={styles.button} onClick={handleSearch}>
+        Go
+      </button>
+    </div>
   );
 }
+
+const styles = {
+  container: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "50px",
+    border: "solid",
+  },
+  input: {
+    padding: "10px",
+    fontSize: "18px",
+    borderRadius: "5px",
+    border: "none",
+    outline: "none",
+    flex: "1",
+  },
+  button: {
+    padding: "10px 20px",
+    backgroundColor: "#4285f4",
+    color: "#fff",
+    fontSize: "18px",
+    borderRadius: "5px",
+    border: "none",
+    outline: "none",
+    cursor: "pointer",
+  },
+};
+//#4285f4
 
 export default SearchBar;
