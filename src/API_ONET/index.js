9a7a668c5a92cb729795dd738d91d3d3e7f26b88
Wesley <brooks.wesley1217@gmail.com>
2023-02-14 12:47:28 -0500

    CareerAPI
    Initial Connection

---
+++ src/API_ONET/index.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export let API_ONET;
+
+export function init_api_onet(id) {
+    const apiOption = {
+        baseURL: `https://services.onetcenter.org/ws/mnm/careers/${id}/report`,
+        timeout: 30000,
+        
+        
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+            'Authorization': 'client=linkedin_company_myn',
+            'Access-Control-Allow-Origin' : '*'
+          }
+    };
+    API_ONET = axios.create(apiOption);
+
+}
--- src/Pages/ExploreCareers/ExploreCareers.jsx
+++ src/Pages/ExploreCareers/ExploreCareers.jsx
@@ -4,6 +4,7 @@ import {init_api, API} from '../../API';
 import CourseCardList from '../../Components/CardList/CourseCardList';
 import Nav from '../../Components/Nav/Nav';
 import "./ExploreCareers.css"
+import { init_api_onet, API_ONET } from '../../API_ONET';
 
 function ExploreCareers() {
 
@@ -11,9 +12,20 @@ function ExploreCareers() {
 
     const courseIDs = [1,2,3,4,5,6];
 
+    const  test = async() => {
+        init_api();
+        await API.get('/api/career/')
+        .then((response) => {
+            console.log(response.data);
+        })
+        
+    }
+
     useEffect(() => {
 
         const getCourseInfo = async() => {
+            
+            /*
             init_api();
             var namesTemp = [];
             var descriptionsTemp = [];
@@ -54,6 +66,7 @@ function ExploreCareers() {
             });
 
             setData(tempData);
+            */
 
 
         }
@@ -82,6 +95,8 @@ function ExploreCareers() {
 
 
             </div>
+
+            <button onClick={test}>Test button</button>
         </div>
     );
 };
